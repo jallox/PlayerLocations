cb6b000633757fc4a0205df968dc88074a680cb4
JayoX-JDT <javiluky2009@gmail.com>
2023-11-17 19:10:37 +0100

    Initial commit

---
+++ src/main/java/dev/jayox/PlayerLocations.java
@@ -0,0 +1,17 @@
+package dev.jayox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerLocations extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
